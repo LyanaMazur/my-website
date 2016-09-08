d37b1bf8b864a409da00b71c8edb4a895e682e4b
Roxolana <qweenlyana@gmail.com>
2016-09-08 11:34:24 +0000

    add new file

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/*$(function() {
 
-	$(".top_nav a").mPageScroll2id({
-		offset: 100
-	});
-});*/
 
 $(document).ready(function(){
     $(".top_nav").on("click","a",function (event){
@@ -14,3 +9,131 @@ $(document).ready(function(){
     $('body,html').animate({scrollTop:top}, 1500);
     });
 });
+
+//for photos
+var imageCount = 10;
+var currentImageIndex;
+
+
+
+$('#myImg1').click(function(){
+    openImage('#myImg1');
+});
+
+$('#myImg2').click(function(){
+    openImage('#myImg2');
+});
+
+$('#myImg3').click(function(){
+    openImage('#myImg3');
+});
+
+$('#myImg4').click(function(){
+    openImage('#myImg4');
+});
+
+$('#myImg5').click(function(){
+    openImage('#myImg5');
+});
+
+$('#myImg6').click(function(){
+    openImage('#myImg6');
+});
+
+$('#myImg7').click(function(){
+    openImage('#myImg7');
+});
+
+$('#myImg8').click(function(){
+    openImage('#myImg8');
+});
+
+$('#myImg9').click(function(){
+    openImage('#myImg9');
+});
+
+$('#myImg10').click(function(){
+    openImage('#myImg10');
+});
+
+
+$(".close").click(function(){
+   $('#myModal').hide();
+});
+
+$(".prev").click(function(){
+    var prevImageIndex;
+    if (currentImageIndex > 1)
+    {
+        prevImageIndex = currentImageIndex - 1;
+    }
+    else
+    {
+        prevImageIndex = imageCount;
+    }
+    
+    openImage("#myImg" + prevImageIndex);
+});
+
+$(".next").click(function(){
+    var nextImageIndex;
+    if (currentImageIndex < imageCount)
+    {
+        nextImageIndex = currentImageIndex + 1;
+    }
+    else
+    {
+        nextImageIndex = 1;
+    }
+    
+    openImage("#myImg" + nextImageIndex);
+});
+
+
+function openImage(id)
+{
+    currentImageIndex = parseInt(id.replace( /^\D+/g, ''));
+    // Get the modal
+var modal = document.getElementById('myModal');
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+var img = $(id);
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.attr("src");
+    captionText.innerHTML = img.attr("alt");
+
+}
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+//span.onclick = function() {
+//    modal.style.display = "none";
+//}
+// -------------------------------------------------------------
+// forms
+$(function(){
+    $('#btn-send').click(sendForm);
+});
+    function sendForm(e) {
+        
+        e.preventDefault();
+        $.ajax({
+        url: "https://formspree.io/qweenlyana@gmail.com", 
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#msg').val()
+        },
+        dataType: "json",
+        success: function() {
+            $('#thanks').html('Thank You for contacting us!');
+        }
+});
+    
+};
